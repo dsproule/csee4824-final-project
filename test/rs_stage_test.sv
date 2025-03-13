6ae59cda7e09016c7ef0858fe58f695ccb8af7fc
yd2770 <yd2770@cadpc20.ee.columbia.edu>
2025-03-13 11:53:52 -0400

    comparison added in rs_stage_test

--- test/rs_stage_test.sv
+++ test/rs_stage_test.sv
@@ -20,6 +20,8 @@ module testbench;
     logic d_stall;
     S_X_PACKET [`RS_SZ-1:0] S_X_pack;
     integer i, j;
+    logic [3:0] clock_count;
+    logic error_count;
 
     rs_stage rs_stage(
         .clock(clock),
@@ -41,10 +43,19 @@ module testbench;
     );
 
     task print_rs;
-        $display("\n(RS_TABLE)\n------------------------------------------");
+        $display("\n(RS_TABLE)\ttime: %d\n------------------------------------------", clock_count - 1);
         for(j = 0; j < `RS_SZ; j=j+1)
             $display("index: %4d   T:%4d   T1:%4d   T2:%4d   V1:%4d   V2:%4d   busy:   %b   ready:%b", j, rs_table[j].T, rs_table[j].T1, rs_table[j].T2, rs_table[j].V1, rs_table[j].V2, busy[j], rs_table[j].ready);
         $display("------------------------------------------");
+    endtask
+
+    task compare;
+        input [15:0] idx, t, t1, t2, v1, v2, busy;
+        if((rs_table[idx].T != t) || (rs_table[idx].T1 != t1) || (rs_table[idx].T2 != t2) || (rs_table[idx].V1 != v1) || (rs_table[idx].V2 != v2)) begin
+            error_count = error_count + 1;
+            $display("@@@error time: %d\t", clock_count - 1);
+            $display("@@@correct answer should be = index: %4d   T:%4d   T1:%4d   T2:%4d   V1:%4d   V2:%4d   busy:   %b", idx, t, t1, t2, v1, v2, busy);
+        end
     endtask
 
     always begin
@@ -52,8 +63,13 @@ module testbench;
         clock = ~clock;
     end
 
+    always@(posedge clock) begin
+        clock_count <= clock_count + 1;
+    end
+
     initial begin
         clock = 0;
+        clock_count = 0;
         reset = 0;
         ID_EX_reg = {
             `NOP, 
@@ -87,8 +103,6 @@ module testbench;
         reset = 0;
         // during reset, clear the reservation table
 
-        print_rs();
-
         // ld X(r4), r2 // 1
         V1 = 0;
         V2 = 8;
@@ -107,6 +121,10 @@ module testbench;
         // expected change
         // index:    0   T:   1   T1:   0   T2:   0   V1:   5   V2:   1   busy:   1   ready:11
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 1, 0, 0, 0, 8, 1);
+        compare(2, 0, 0, 0, 0, 0, 0);
+        compare(3, 0, 0, 0, 0, 0, 0);
 
         // mul r1, r2, r3 // 2
         V1 = 5;
@@ -124,11 +142,15 @@ module testbench;
         cdb.V = 0;
         @(negedge clock);
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 0, 0, 0, 0, 0, 0);
+        compare(3, 2, 0, 1, 5, 0, 1);
 
         // st r3, Z(r4) // 3
         V1 = 0;
         V2 = 8;
-        T = 0;
+        T = 3;
         ID_EX_reg.rs_idx = 2;
         S_X_reg[0].ready = 0;
         S_X_reg[1].ready = 1;
@@ -141,6 +163,10 @@ module testbench;
         cdb.V = 0;
         @(negedge clock);
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 3, 2, 0, 0, 8, 1);
+        compare(3, 2, 0, 1, 5, 0, 1);
 
         // addi r4, 4, r4 // 4
         V1 = 8;
@@ -158,6 +184,10 @@ module testbench;
         cdb.V = 10;
         @(negedge clock);
         print_rs();
+        compare(0, 4, 0, 0, 8, 0, 1);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 3, 2, 0, 0, 8, 1);
+        compare(3, 0, 0, 0, 0, 0, 0);
 
         // ldf X(r4), r2 // 5
         V1 = 0;
@@ -175,6 +205,10 @@ module testbench;
         cdb.V = 0;
         @(negedge clock);
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 5, 0, 4, 0, 0, 1);
+        compare(2, 3, 2, 0, 0, 8, 1);
+        compare(3, 0, 0, 0, 0, 0, 0);
 
         // mul r1, r2, r3 // 6
         V1 = 5;
@@ -184,7 +218,7 @@ module testbench;
         S_X_reg[0].ready = 0;
         S_X_reg[1].ready = 1;
         S_X_reg[2].ready = 1;
-        S_X_reg[3].ready = 1;
+        S_X_reg[3].ready = 0;
         T1 = {0, 1'b0};
         T2 = {5, 1'b0};
         cdb.valid = 0;
@@ -192,25 +226,65 @@ module testbench;
         cdb.V = 0;
         @(negedge clock);
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 5, 0, 4, 0, 0, 1);
+        compare(2, 3, 2, 0, 0, 8, 1);
+        compare(3, 6, 0, 5, 5, 0, 1);
 
-        // mul r1, r2, r3 // 6
-        V1 = 5;
-        V2 = 0;
-        T = 6;
-        ID_EX_reg.rs_idx = 3;
-        S_X_reg[0].ready = 0;
+        // 7
+        cdb.valid = 1;
+        cdb.T = 4;
+        cdb.V = 12;
+        S_X_reg[0].ready = 1;
         S_X_reg[1].ready = 1;
         S_X_reg[2].ready = 1;
-        S_X_reg[3].ready = 1;
-        T1 = {0, 1'b0};
-        T2 = {5, 1'b0};
-        cdb.valid = 0;
-        cdb.T = 0;
-        cdb.V = 0;
+        S_X_reg[3].ready = 0;
         @(negedge clock);
         print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 3, 2, 0, 0, 8, 1);
+        compare(3, 6, 0, 5, 5, 0, 1);
+
+        // 8
+        cdb.valid = 1;
+        cdb.T = 2;
+        cdb.V = 11;
+        @(negedge clock);
+        print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 0, 0, 0, 0, 0, 0);
+        compare(3, 6, 0, 5, 5, 0, 1);
+    
+        // st r3, Z(r4) // 9
+        V1 = 0;
+        V2 = 1;
+        T = 7;
+        ID_EX_reg.rs_idx = 2;
+        S_X_reg[0].ready = 1;
+        S_X_reg[1].ready = 1;
+        S_X_reg[2].ready = 0;
+        S_X_reg[3].ready = 1;
+        T1 = {6, 1'b0};
+        T2 = {0, 1'b0};
+        cdb.valid = 1;
+        cdb.T = 5;
+        cdb.V = 15;
+        @(negedge clock);
+        print_rs();
+        compare(0, 0, 0, 0, 0, 0, 0);
+        compare(1, 0, 0, 0, 0, 0, 0);
+        compare(2, 7, 6, 0, 0, 1, 1);
+        compare(3, 0, 0, 0, 0, 0, 0);
 
         @(negedge clock);
+        if(error_count == 0) begin
+            $display("@@@correct");
+        end
+        else begin
+            $display("@@@error");
+        end
         @(negedge clock);
         $finish;
     end
