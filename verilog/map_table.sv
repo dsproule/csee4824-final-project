c4d7ed93c1a60c1bfa1f21a9fa185d4854b465d9
Nicolas Alarcon <na2946@columbia.edu>
2025-03-25 13:55:08 -0400

    update map table to be combinational

--- verilog/map_table.sv
+++ verilog/map_table.sv
@@ -13,8 +13,6 @@ module map_table (
     logic retire_entry;
     MT_ENTRY mt_table [31:0]; 
 
-    MT_ENTRY next_T1, next_T2;
-
     assign retire_entry = (mt_table[retire_r].T == retire_T) && (retire_r != r);
 
     // forwards signal that rob has value present if cdb collides
@@ -24,18 +22,18 @@ module map_table (
             mt_table_out[i * $bits(MT_ENTRY) +: $bits(MT_ENTRY)] = mt_table[i];
         end
 
-        next_T1 = 0;
-        next_T1 = 0;
+        T1 = 0;
+        T2 = 0;
         
         //functionally conditionals should never both be true
-        next_T1 = (cdb.valid && (cdb.T == mt_table[r1].T)) ? {mt_table[r1].T, `TRUE} : mt_table[r1]; //functionally conditionals should never both be true
-        next_T2 = (cdb.valid && (cdb.T == mt_table[r2].T)) ? {mt_table[r2].T, `TRUE} : mt_table[r2];
+        T1 = (cdb.valid && (cdb.T == mt_table[r1].T)) ? {mt_table[r1].T, `TRUE} : mt_table[r1]; //functionally conditionals should never both be true
+        T2 = (cdb.valid && (cdb.T == mt_table[r2].T)) ? {mt_table[r2].T, `TRUE} : mt_table[r2];
 
         if (retire_entry && (retire_r == r1)) begin
-            next_T1 = 0;
+            T1 = 0;
         end
         if (retire_entry && (retire_r == r2)) begin
-            next_T2 = 0;
+            T2 = 0;
         end
     end
     
@@ -47,8 +45,6 @@ module map_table (
             end
         end else if (en) begin
             mt_table[r] <= (r != 0) ? {T, `FALSE} : 0;
-            T1 <= next_T1;
-            T2 <= next_T2;
 
             // clears a tag if its not being reassigned
             if (retire_entry) begin
