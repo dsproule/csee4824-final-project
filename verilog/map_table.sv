ec161a506ce8d575693d918737f23e0695f54a60
AliceDu2002 <alicedu0211@gmail.com>
2025-03-25 13:40:43 -0400

    rs stage + map table finished

--- verilog/map_table.sv
+++ verilog/map_table.sv
@@ -12,23 +12,21 @@ module map_table (
     logic [5:0] reset_idx, cdb_idx;
     logic retire_entry;
 
-    MT_ENTRY next_T1, next_T2;
-
     assign retire_entry = (mt_table[retire_r].T == retire_T) && (retire_r != r);
 
     // forwards signal that rob has value present if cdb collides
     always_comb begin
-        next_T1 = 0;
-        next_T1 = 0;
+        T1 = 0;
+        T1 = 0;
         
         //functionally conditionals should never both be true
-        next_T1 = (cdb.valid && (cdb.T == mt_table[r1].T)) ? {mt_table[r1].T, `TRUE} : mt_table[r1]; //functionally conditionals should never both be true
-        next_T2 = (cdb.valid && (cdb.T == mt_table[r2].T)) ? {mt_table[r2].T, `TRUE} : mt_table[r2];
+        T1 = (cdb.valid && (cdb.T == mt_table[r1].T)) ? {mt_table[r1].T, `TRUE} : mt_table[r1]; //functionally conditionals should never both be true
+        T2 = (cdb.valid && (cdb.T == mt_table[r2].T)) ? {mt_table[r2].T, `TRUE} : mt_table[r2];
 
         if (retire_entry && (retire_r == r1)) 
-            next_T1 = 0;
+            T1 = 0;
         if (retire_entry && (retire_r == r2)) 
-            next_T2 = 0;
+            T2 = 0;
 
     end
     
@@ -39,8 +37,6 @@ module map_table (
                 mt_table[reset_idx] <= 0;
         end else begin
             mt_table[r] <= (r != 0) ? {T, `FALSE} : 0;
-            T1 <= next_T1;
-            T2 <= next_T2;
 
             // clears a tag if its not being reassigned
             if (retire_entry)
