5ad6edfe342310beb0041816da31b2c9bdaaaf60
AliceDu2002 <alicedu0211@gmail.com>
2025-03-28 04:05:12 -0400

    multi_module_test clean

--- test/multi_module_test.sv
+++ test/multi_module_test.sv
@@ -3,139 +3,82 @@
 // rename to 'rs_stage_test.sv' and run 'make rs_stage.out' to run
 
 module testbench;
-    // Inputs
+    // General Inputs
     logic clock;
     logic reset;
     logic en;
-    logic d_valid;
-    logic [`XLEN-1:0] V1, V2;           // values from ROB/regfile
-    CDB cdb;
-    D_S_PACKET D_S_reg;
-    S_X_PACKET [`RS_SZ-1:0] S_X_reg;    // commited reg value passing back
-    ROB_T T;
-    ROB_T T_wire;   // from ROB to rs/map table
-    // MT_ENTRY T1, T2;                    
-    MT_ENTRY T1_wire, T2_wire;                    // from map table
+    CDB cdb;                    // manually assigned value (should come from later stage)
+    D_S_PACKET D_S_reg;         // manually assigned value (should come from previous stage)
+    logic [4:0] r, r1, r2;      // manually assigned value (should come from previous stage)
+    logic [`RS_SZ-1:0] FU_ready;// manually assigned value (should come from functional units)
+
+    // General Outputs
+    S_X_PACKET [`RS_SZ-1:0] S_X_pack; // for next stage
+
+    // Tag
+    ROB_T T_wire;               // Destination tag: from ROB -> Map Table & RS
+    MT_ENTRY T1_wire, T2_wire;  // Source tag: from Map Table -> RS & ROB & Regfile  
+
+    // Value
+    logic [`XLEN-1:0] V1_rs, V2_rs;             // Final value sent into RS
+    logic [`XLEN-1:0] V1_rob, V2_rob;           // used when mt_table[i].plus == 1
+    logic [`XLEN-1:0] regfile_V1, regfile_V2;   // used when mt_table[i].T == 0
+    assign V1_rs = (T1_wire.plus) ? V1_rob : regfile_V1;
+    assign V2_rs = (T2_wire.plus) ? V2_rob : regfile_V2;
+
+    // Retire
+    logic retire;
+    ROB_T retire_T_wire;        // from ROB -> Map Table & Regfile
+    logic [4:0] retire_r_wire;  // from ROB -> Map Table & Regfile
+
+    // Reservation Station Signals
     RS_ENTRY [ `RS_SZ-1:0] rs_table;
-    logic [`RS_SZ-1:0] busy;
-    logic [4:0] r, r1, r2; // the original register value
-    ROB_T retire_T;
-    ROB_T retire_T_wire;
-    logic [4:0] retire_r;
-    logic [4:0] retire_r_wire;
+    logic [`RS_SZ-1:0] busy;    // functional unit in used -> busy
+    logic d_stall;              // If that functional unit is occupied -> stall
+    
+    // Map Table
     MT_ENTRY mt_table [31:0];
 
-    // Outputs
-    logic d_stall;
-    S_X_PACKET [`RS_SZ-1:0] S_X_pack;
-    integer i, j, k, l, m, n;
-    logic [7:0] clock_count;
-    logic [5:0] error_count;
-    logic [`RS_SZ-1:0] FU_ready;
-
-    // for rob
+    // ROB Signals
     logic full;
     logic empty;
-    logic retire;
-    logic [4:0] regfile_write_idx;
-    logic [`XLEN-1:0] regfile_write_data;
-    logic [`XLEN-1:0] V1_rob, V2_rob;
-    logic [$bits(ROB_ENTRY)*`ROB_SZ-1:0] rob_table_out;
+    logic [$bits(ROB_ENTRY)*`ROB_SZ-1:0] rob_table_out; // original ROB output
+    ROB_ENTRY rob_table [`ROB_SZ-1:0];                  // formatted ROB
 
-    // for handcraft value
-    logic ppl_write_en;
-    logic [4:0] ppl_write_idx;
-    logic [`XLEN-1:0] ppl_write_data;
-
-    // for rob output
-    logic rob_write_en;
-    logic [4:0] rob_write_idx;
-    logic [`XLEN-1:0] rob_write_data;
-
+    // Regfile Write Signals
+    logic [4:0] regfile_write_idx;          // Final idx to Regfile
+    logic [`XLEN-1:0] regfile_write_data;   // Final data to Regfile
+    logic regfile_write_en;                 // Final en to Regfile
+    logic rob_write_en;                     // ROB -> Regfile
+    logic [4:0] rob_write_idx;              // ROB -> Regfile
+    logic [`XLEN-1:0] rob_write_data;       // ROB -> Regfile
+    logic ppl_write_en;                     // initialization
+    logic [4:0] ppl_write_idx;              // initialization
+    logic [`XLEN-1:0] ppl_write_data;       // initialization
     assign regfile_write_en = en ? retire : ppl_write_en;
     assign regfile_write_data = en ? rob_write_data : ppl_write_data;
     assign regfile_write_idx = en ? retire_r_wire : ppl_write_idx;
 
-    logic [`XLEN-1:0] V1_rs, V2_rs;
-    logic [`XLEN-1:0] regfile_V1, regfile_V2;
-    MT_ENTRY mt_table [31:0];
-    ROB_ENTRY rob_table [`ROB_SZ-1:0];
+    // Count
+    integer i, j, k, l, m, n;
+    logic [7:0] clock_count;
+    logic [5:0] error_count;
 
-    assign V1_rs = (T1_wire.plus) ? V1_rob : regfile_V1;
-    assign V2_rs = (T2_wire.plus) ? V2_rob : regfile_V2;
+    map_table map_table_inst (.clock(clock), .reset(reset), .r(r), .r1(r1), .r2(r2), .retire_r(retire_r_wire), 
+                              .cdb(cdb), .T(T_wire), .retire_T(retire_T_wire), .T1(T1_wire), .T2(T2_wire), .mt_table(mt_table));
 
-    map_table map_table(
-        .clock(clock),
-        .reset(reset),
-        // .en(en),
-        .r(r), // 5 bits // finished
-        .r1(r1), // 5 bits // finished 
-        .r2(r2), // 5 bits // finished
-        .retire_r(retire_r_wire), // 5 bits
-        .cdb(cdb), // CDB
-        .T(T_wire), // ROB_T
-        .retire_T(retire_T_wire), // ROB_T
-        .T1(T1_wire), // MT_ENTRY // output // finished 
-        .T2(T2_wire), // MT_ENTRY // output // finished
-        .mt_table(mt_table)
-    );
+    rs_stage rs_stage_inst (.clock(clock), .reset(reset), .en(en), .cdb(cdb), .D_S_reg(D_S_reg), .FU_ready(FU_ready),
+                            .T(T_wire), .T1(T1_wire), .T2(T2_wire), .V1(V1_rs), .V2(V2_rs), .d_stall(d_stall),
+                            .S_packet(S_X_pack), .rs_table(rs_table), .busy(busy));
 
-    rs_stage rs_stage(
-        .clock(clock),
-        .reset(reset),
-        .en(en),
-        .cdb(cdb),
-        .D_S_reg(D_S_reg),
-        // .S_X_reg(S_X_reg), 
-        .FU_ready(FU_ready),
-        .T(T_wire),
-        .T1(T1_wire),
-        .T2(T2_wire),
-        .V1(V1_rs),
-        .V2(V2_rs),
+    rob rob_inst (.clock(clock), .reset(reset), .r(r), .T1(T1_wire.T), .T2(T2_wire.T), .cdb(cdb),
+                  .dispatch_valid(D_S_reg.valid & ~d_stall), .T(T_wire), .retire_T_out(retire_T_wire), 
+                  .ppl_ctrl(), .full(full), .empty(empty), .retire(retire), .regfile_write_idx_out(retire_r_wire), 
+                  .V1(V1_rob), .V2(V2_rob), .regfile_write_data(rob_write_data),.rob_table_out(rob_table_out));
 
-        .d_stall(d_stall),
-        .S_packet(S_X_pack),
-        .rs_table(rs_table),
-        .busy(busy)
-    );
-
-    rob rob_inst (
-        .clock(clock), 
-        .reset(reset), 
-        .r(r), 
-        .T1(T1_wire.T), // for getting the ROB value
-        .T2(T2_wire.T), // for getting the ROB value
-        .cdb(cdb),
-        .dispatch_valid(D_S_reg.valid & ~d_stall), // ?
-        .T(T_wire), 
-        .retire_T_out(retire_T_wire), //TODO
-        .ppl_ctrl(), //this has fields for mispredicted branches to be used elsewhere in ppl
-        .full(full), 
-        .empty(empty),
-        .retire(retire), 
-        .regfile_write_idx_out(retire_r_wire), //TODO
-        .V1(V1_rob), 
-        .V2(V2_rob), 
-        .regfile_write_data(rob_write_data),
-        .rob_table_out(rob_table_out)
-    );
-
-    regfile regfile_0(
-        // Inputs
-        .clock(clock),
-        .read_idx_1(r1), .read_idx_2(r2), .write_idx(regfile_write_idx),
-        .write_en(regfile_write_en),
-        .write_data(regfile_write_data),
-        // Outputs
-        .read_out_1(regfile_V1), .read_out_2(regfile_V2)
-    );
-
-    always_comb begin
-        for (int i = 0; i < `ROB_SZ; i++) begin
-            rob_table[i] = rob_table_out[i * $bits(ROB_ENTRY) +: $bits(ROB_ENTRY)];
-        end
-    end
+    regfile regfile_inst (.clock(clock), .read_idx_1(r1), .read_idx_2(r2), .write_idx(regfile_write_idx),
+                          .write_en(regfile_write_en),.write_data(regfile_write_data),
+                          .read_out_1(regfile_V1), .read_out_2(regfile_V2));
 
     task print_rs;
         $display("\n(RS_TABLE)\ttime: %d\n------------------------------------------", clock_count - 6);
@@ -158,14 +101,14 @@ module testbench;
         $display("------------------------------------------");
     endtask
 
-    task compare;
+    task compare; // compare rs
         input [15:0] idx, t, t1, t2, v1, v2;
         input busy;
         if((rs_table[idx].T != t) || (rs_table[idx].T1 != t1) || (rs_table[idx].T2 != t2) || (rs_table[idx].V1 != v1) || (rs_table[idx].V2 != v2) || (busy[idx] != busy)) begin
             error_count = error_count + 1;
             $display("@@@Failed at time: %d\t", clock_count - 6);
             $display("@@@correct answer should be = index: %4d   T:%4d   T1:%4d   T2:%4d   V1:%4d   V2:%4d   busy:%b", idx, t, t1, t2, v1, v2, busy);
-            // $finish;
+            $finish;
         end
     endtask
 
@@ -175,7 +118,7 @@ module testbench;
             error_count = error_count + 1;
             $display("@@@Failed at time: %d\t", clock_count - 6);
             $display("@@@stall error: d_stall: %b", d_stall);
-            // $finish;
+            $finish;
         end
     endtask
 
@@ -185,7 +128,7 @@ module testbench;
             error_count = error_count + 1;
             $display("@@@Failed at time: %d\t", clock_count - 6);
             $display("@@@correct answer should be = index: %4d   T:%4d   plus:%4d", idx, t, plus);
-            // $finish;
+            $finish;
         end
     endtask
 
@@ -195,20 +138,28 @@ module testbench;
             error_count = error_count + 1;
             $display("@@@Failed at time: %d\t", clock_count - 6);
             $display("@@@correct answer should be = index: %4d   r:%4d   V:%4d", idx, r, V);
-            // $finish;
+            $finish;
         end
     endtask
 
+    // Clock Generation
     always begin
         #(`CLOCK_PERIOD/2.0);
         clock = ~clock;
     end
-
     always@(posedge clock) begin
         clock_count <= clock_count + 1;
     end
 
+    // ROB Table formatting
+    always_comb begin
+        for (int i = 0; i < `ROB_SZ; i++) begin
+            rob_table[i] = rob_table_out[i * $bits(ROB_ENTRY) +: $bits(ROB_ENTRY)];
+        end
+    end
+
     initial begin
+        // reset
         clock = 0;
         error_count = 0;
         clock_count = 0;
@@ -233,17 +184,10 @@ module testbench;
             1'b0, // csr_op
             1'b1  // valid
         };
-        V1 = 0;
-        V2 = 0;
         cdb = 0;
-        T = 0;
-        // T1 = 0;
-        // T2 = 0;
         r = 0;
         r1 = 0;
         r2 = 0;
-        retire_r = 0;
-        retire_T = 0;
         @(negedge clock);
         reset = 1;
         ppl_write_en = 1;
@@ -263,22 +207,16 @@ module testbench;
         ppl_write_data = 8;
         @(negedge clock);
         reset = 0;
-        // during reset, clear the reservation table
+
 
         @(negedge clock); 
         en = 1;
         // ld X(r4), r2 // 1
-        // V1 = 0;
-        // V2 = 8;
-        // T = 1;
+        // V1 = 0; V2 = 8; T = 1; T1 = {0, 1'b0}; T2 = {0, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 1;
-        // T1 = {0, 1'b0};
-        // T2 = {0, 1'b0};
         r = 2;
         r1 = 0;
         r2 = 4;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
@@ -308,19 +246,12 @@ module testbench;
         compare_rob(6, 0, 0);
         compare_rob(7, 0, 0);
 
-        // @(negedge clock); 
         // mul r1, r2, r3 // 2
-        // V1 = 5;
-        // V2 = 0;
-        // T = 2;
+        // V1 = 5; V2 = 0; T = 2; T1 = {0, 1'b0}; T2 = {1, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 3;
-        // T1 = {0, 1'b0};
-        // T2 = {1, 1'b0};
         r = 3;
         r1 = 1;
         r2 = 2;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
@@ -351,19 +282,12 @@ module testbench;
         compare_rob(6, 0, 0);
         compare_rob(7, 0, 0);
 
-        // @(negedge clock);
-        // st r3, Z(r4) // 3
-        // V1 = 0;
-        // V2 = 8;
-        // T = 3;
+        // stf r3, Z(r4) // 3
+        // V1 = 0; V2 = 8; T = 3; T1 = {2, 1'b0}; T2 = {0, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 2;
-        // T1 = {2, 1'b0};
-        // T2 = {0, 1'b0};
         r = 0; // question: what should be the r for store function
         r1 = 3;
         r2 = 4;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
@@ -394,20 +318,12 @@ module testbench;
         compare_rob(6, 0, 0);
         compare_rob(7, 0, 0);
         
-    
-        // @(negedge clock); 
         // addi r4, 4, r4 // 4
-        // V1 = 8;
-        // V2 = 0;
-        // T = 4;
+        // V1 = 8; V2 = 0; T = 4; T1 = {0, 1'b0}; T2 = {0, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 0;
-        // T1 = {0, 1'b0};
-        // T2 = {0, 1'b0};
         r = 4;
         r1 = 4;
         r2 = 0;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 1;
         cdb.T = 1;
         cdb.V = 10;
@@ -438,19 +354,12 @@ module testbench;
         compare_rob(6, 0, 0);
         compare_rob(7, 0, 0);
 
-        // @(negedge clock); 
         // ldf X(r4), r2 // 5
-        // V1 = 0;
-        // V2 = 0;
-        // T = 5;
+        // V1 = 0; V2 = 0; T = 5; T1 = {0, 1'b0}; T2 = {4, 1'b0}; retire_r = 2; retire_T = 1;
         D_S_reg.rs_idx = 1;
-        // T1 = {0, 1'b0};
-        // T2 = {4, 1'b0};
         r = 2;
         r1 = 0;
         r2 = 4;
-        // retire_r = 2;
-        // retire_T = 1;
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
@@ -480,21 +389,13 @@ module testbench;
         compare_rob(5, 2, 0);
         compare_rob(6, 0, 0);
         compare_rob(7, 0, 0);
-        
 
-        // @(negedge clock); 
         // mul r1, r2, r3 // 6
-        // V1 = 5;
-        // V2 = 0;
-        // T = 6;
+        // V1 = 5; V2 = 0; T = 6; T1 = {0, 1'b0}; T2 = {5, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 3;
-        // T1 = {0, 1'b0};
-        // T2 = {5, 1'b0};
         r = 3;
         r1 = 1;
         r2 = 2;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
@@ -525,20 +426,12 @@ module testbench;
         compare_rob(6, 3, 0);
         compare_rob(7, 0, 0);
         
-
-        // @(negedge clock); 
-        // 7
-        // V1 = 0;
-        // V2 = 1;
-        // T = 7;
+        // stf r3, Z(r4) (stall) // 7
+        // V1 = 0; V2 = 1; T = 7; T1 = {6, 1'b0}; T2 = {0, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 2;
-        // T1 = {6, 1'b0};
-        // T2 = {0, 1'b0};
         r = 0;
         r1 = 3;
         r2 = 4;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 1;
         cdb.T = 4;
         cdb.V = 12;
@@ -569,19 +462,12 @@ module testbench;
         compare_rob(6, 3, 0);
         compare_rob(7, 0, 0);
         
-        // @(negedge clock); 
-        // 8
-        // V1 = 0;
-        // V2 = 1;
-        // T = 7;
+        // stf r3, Z(r4) (stall) // 8
+        // V1 = 0; V2 = 1; T = 7; T1 = {6, 1'b0}; T2 = {0, 1'b0}; retire_r = 0; retire_T = 0;
         D_S_reg.rs_idx = 2;
-        // T1 = {6, 1'b0};
-        // T2 = {0, 1'b0};
         r = 0;
         r1 = 3;
         r2 = 4;
-        // retire_r = 0;
-        // retire_T = 0;
         cdb.valid = 1;
         cdb.T = 2;
         cdb.V = 11;
@@ -612,20 +498,12 @@ module testbench;
         compare_rob(6, 3, 0);
         compare_rob(7, 0, 0);
         
-    
-        // st r3, Z(r4) // 9
-        // @(negedge clock); 
-        // V1 = 0;
-        // V2 = 2;
-        // T = 7;
+        // stf r3, Z(r4) // 9
+        // V1 = 0; V2 = 2; T = 7; T1 = {6, 1'b0}; T2 = {4, 1'b1}; retire_r = 3; retire_T = 2;
         D_S_reg.rs_idx = 2;
-        // T1 = {6, 1'b0};
-        // T2 = {4, 1'b1};
         r = 0;
         r1 = 3;
         r2 = 4;
-        // retire_r = 3;
-        // retire_T = 2; // question: do store inst need to retire? YES - Nico
         cdb.valid = 1;
         cdb.T = 5;
         cdb.V = 15;
@@ -657,18 +535,10 @@ module testbench;
         compare_rob(7, 0, 0);
 
         // none // 10
-        // @(negedge clock); 
-        // V1 = 0;
-        // V2 = 0;
-        // T = 7;
         D_S_reg.rs_idx = 0;
-        // T1 = {6, 1'b0};
-        // T2 = {4, 1'b1};
         r = 0;
         r1 = 0;
         r2 = 0;
-        // retire_r = 0;
-        // retire_T = 0; // question: do store inst need to retire?
         cdb.valid = 0;
         cdb.T = 0;
         cdb.V = 0;
