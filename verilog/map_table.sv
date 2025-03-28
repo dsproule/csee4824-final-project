e30d2be3035142ebb39af1ce0138c1efd10af52a
Nicolas Alarcon <na2946@columbia.edu>
2025-03-28 13:56:37 -0400

    map table tb updated to match working config, added en and map_table_out for synth debugging

--- verilog/map_table.sv
+++ verilog/map_table.sv
@@ -1,21 +1,26 @@
 `include "verilog/sys_defs.svh"
 
 module map_table (
-    input clock, reset,
+    input clock, reset, en,
     input [4:0] r, r1, r2, retire_r,
     input CDB   cdb,
     input ROB_T T, retire_T,
     
-    output MT_ENTRY T1, T2,
-    output MT_ENTRY mt_table [31:0]
+    output MT_ENTRY T1, T2, 
+    output logic [$bits(MT_ENTRY)*32-1:0] mt_table_out
 );
     logic [5:0] reset_idx, cdb_idx;
     logic retire_entry;
+    MT_ENTRY mt_table [31:0];
 
     assign retire_entry = (mt_table[retire_r].T == retire_T) && (retire_r != r);
 
     // forwards signal that rob has value present if cdb collides
     always_comb begin
+        for (int i = 0; i < 32; i++) begin
+            mt_table_out[i * $bits(MT_ENTRY) +: $bits(MT_ENTRY)] = mt_table[i];
+        end
+
         T1 = 0;
         T1 = 0;
         
@@ -35,7 +40,7 @@ module map_table (
         if (reset) begin
             for (reset_idx = 0; reset_idx < 32; reset_idx++)
                 mt_table[reset_idx] <= 0;
-        end else begin
+        end else if (en) begin
             mt_table[r] <= (r != 0) ? {T, `FALSE} : 0;
 
             // clears a tag if its not being reassigned
