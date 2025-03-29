dc647ee4077f1dfc0036258bff4808ce5e9880be
Donovan <Donovan.sproule@yahoo.com>
2025-03-29 18:30:30 -0400

    doesnt work but does compile

--- test/if_stage_test.sv
+++ test/if_stage_test.sv
@@ -29,52 +29,52 @@ module testbench_chunk;
     logic Icache_valid;
     IF_ID_PACKET IF_ID_reg, IF_packet;
     logic [`XLEN-1:0] Icache2mem_addr, proc2Icache_addr;
-    logic [63:0] mem2Icache_data, Icache2proc_data;
+    logic [63:0] mem2Icache_data, Icache2proc_data, Icache2mem_data;
     logic [1:0] Icache2mem_command;
     logic [3:0] mem2Icache_response, mem2Icache_tag;
 
     mem memory(
-    .clk(clock),
-    .proc2mem_addr(Icache2mem_addr),
-    .proc2mem_data(),                       // icache doesnt store any data
-    .proc2mem_command(Icache2mem_command),  // (these will get muxed/used when we have an LSQ)
+        .clk(clock),
+        .proc2mem_addr(Icache2mem_addr),
+        .proc2mem_data(Icache2mem_data),                       // icache doesnt store any data
+        .proc2mem_command(Icache2mem_command),  // (these will get muxed/used when we have an LSQ)
 
-    .mem2proc_response(mem2Icache_response),
-    .mem2proc_data(mem2Icache_data),
-    .mem2proc_tag(mem2Icache_tag)
-);
+        .mem2proc_response(mem2Icache_response),
+        .mem2proc_data(mem2Icache_data),
+        .mem2proc_tag(mem2Icache_tag)
+    );
     
-    icache icache_0 (
-        // Inputs
-        .clock(clock), .reset(reset),
+    // icache icache_0 (
+    //     // Inputs
+    //     .clock(clock), .reset(reset),
         
-        .Imem2proc_response(mem2Icache_response),
-        .Imem2proc_data(mem2Icache_data),
-        .Imem2proc_tag(mem2Icache_tag),
+    //     .Imem2proc_response(mem2Icache_response),
+    //     .Imem2proc_data(mem2Icache_data),
+    //     .Imem2proc_tag(mem2Icache_tag),
         
-        .proc2Icache_addr(proc2Icache_addr),
+    //     .proc2Icache_addr(proc2Icache_addr),
 
-        // Outputs
-        .proc2Imem_command(Icache2mem_command),
-        .proc2Imem_addr(Icache2mem_addr),
+    //     // Outputs
+    //     .proc2Imem_command(Icache2mem_command),
+    //     .proc2Imem_addr(Icache2mem_addr),
 
-        .Icache_data_out(Icache2proc_data),
-        .Icache_valid_out(Icache_valid)
-    );
+    //     .Icache_data_out(Icache2proc_data),
+    //     .Icache_valid_out(Icache_valid)
+    // );
 
-    if_stage if_stage_0(
-        // Inputs
-        .clock (clock),
-        .reset (reset),
-        .if_valid       (Icache_valid),
-        .take_branch    (),                 // ignore because this scares me for now
-        .branch_target  (),                 // check above comment
-        .Imem2proc_data (Icache2proc_data),
+    // if_stage if_stage_0(
+    //     // Inputs
+    //     .clock (clock),
+    //     .reset (reset),
+    //     .if_valid       (Icache_valid),
+    //     .take_branch    (),                 // ignore because this scares me for now
+    //     .branch_target  (),                 // check above comment
+    //     .Imem2proc_data (Icache2proc_data),
 
-        // Outputs
-        .if_packet      (IF_packet),
-        .proc2Imem_addr (proc2Icache_addr)
-    );
+    //     // Outputs
+    //     .if_packet      (IF_packet),
+    //     .proc2Imem_addr (proc2Icache_addr)
+    // );
 
     initial begin
         forever #(`CLOCK_PERIOD / 2.0) clock = ~clock;
@@ -87,9 +87,21 @@ module testbench_chunk;
         reset = 1;
 
         @(negedge clock);
-        @(negedge clock);
         reset = 0;
 
+        Icache2mem_addr = `XLEN'd0;
+        Icache2mem_data = 64'd100;
+        Icache2mem_command = BUS_STORE;
+        @(negedge clock);
+        Icache2mem_addr = `XLEN'd4;
+        Icache2mem_data = 64'd200;
+        Icache2mem_command = BUS_STORE;
+        @(negedge clock);
+        Icache2mem_addr = `XLEN'd8;
+        Icache2mem_data = 64'd300;
+        Icache2mem_command = BUS_STORE;
+        @(negedge clock);
+        Icache2mem_command = BUS_NONE;
         @(negedge clock);
         @(negedge clock);
         @(negedge clock);
@@ -97,6 +109,10 @@ module testbench_chunk;
         @(negedge clock);
         @(negedge clock);
         @(negedge clock);
+        @(negedge clock);
+        @(negedge clock);
+        @(negedge clock);
+
 
         $finish;
     end
