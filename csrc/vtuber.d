4b3433967c9a037c85161a9c644bcaaa07688288
am6542 <am6542@micro01.ee.columbia.edu>
2025-04-02 17:15:52 -0400

    RS in miscellanous for now, need to connect through cpp

---
+++ csrc/vtuber.d
@@ -0,0 +1,7 @@
+vtuber.o: ../test/vtuber.cpp \
+ /tools/synopsys/vcs/U-2023.03-SP2-1/include/DirectC.h \
+ /user/stud/fall24/am6542/.local/include/ncursesw/curses.h \
+ /user/stud/fall24/am6542/.local/include/ncursesw/ncursesw/ncurses_dll.h \
+ /user/stud/fall24/am6542/.local/include/ncursesw/ncursesw/unctrl.h \
+ /user/stud/fall24/am6542/.local/include/ncursesw/ncursesw/curses.h \
+ ../test/riscv_inst.h
--- test/vtuber_test.sv
+++ test/vtuber_test.sv
@@ -36,6 +36,12 @@ module testbench;
     logic [3:0]       mem2proc_response;
     logic [63:0]      mem2proc_data;
     logic [3:0]       mem2proc_tag;
+
+    RS_ENTRY [`RS_SZ-1:0] rs_table_dbg;
+    X_C_PACKET [`RS_SZ-1:0] X_packets_dbg;
+    CDB cdb_dbg;
+    logic [`RS_SZ-1:0] busy_dbg;
+
 `ifndef CACHE_MODE
     MEM_SIZE          proc2mem_size;
 `endif
@@ -47,21 +53,21 @@ module testbench;
     logic             pipeline_commit_wr_en;
     logic [`XLEN-1:0] pipeline_commit_NPC;
 
-    logic [`XLEN-1:0] if_NPC_dbg;
-    logic [31:0]      if_inst_dbg;
-    logic             if_valid_dbg;
-    logic [`XLEN-1:0] if_id_NPC_dbg;
-    logic [31:0]      if_id_inst_dbg;
-    logic             if_id_valid_dbg;
-    logic [`XLEN-1:0] id_ex_NPC_dbg;
-    logic [31:0]      id_ex_inst_dbg;
-    logic             id_ex_valid_dbg;
-    logic [`XLEN-1:0] ex_mem_NPC_dbg;
-    logic [31:0]      ex_mem_inst_dbg;
-    logic             ex_mem_valid_dbg;
-    logic [`XLEN-1:0] mem_wb_NPC_dbg;
-    logic [31:0]      mem_wb_inst_dbg;
-    logic             mem_wb_valid_dbg;
+    // logic [`XLEN-1:0] if_NPC_dbg;
+    // logic [31:0]      if_inst_dbg;
+    // logic             if_valid_dbg;
+    // logic [`XLEN-1:0] if_id_NPC_dbg;
+    // logic [31:0]      if_id_inst_dbg;
+    // logic             if_id_valid_dbg;
+    // logic [`XLEN-1:0] id_ex_NPC_dbg;
+    // logic [31:0]      id_ex_inst_dbg;
+    // logic             id_ex_valid_dbg;
+    // logic [`XLEN-1:0] ex_mem_NPC_dbg;
+    // logic [31:0]      ex_mem_inst_dbg;
+    // logic             ex_mem_valid_dbg;
+    // logic [`XLEN-1:0] mem_wb_NPC_dbg;
+    // logic [31:0]      mem_wb_inst_dbg;
+    // logic             mem_wb_valid_dbg;
 
 
     // Instantiate the Pipeline
@@ -77,7 +83,9 @@ module testbench;
         .proc2mem_command (proc2mem_command),
         .proc2mem_addr    (proc2mem_addr),
         .proc2mem_data    (proc2mem_data),
+    `ifndef CACHE_MODE
         .proc2mem_size    (proc2mem_size),
+    `endif
 
         .pipeline_completed_insts (pipeline_completed_insts),
         .pipeline_error_status    (pipeline_error_status),
@@ -86,21 +94,26 @@ module testbench;
         .pipeline_commit_wr_en    (pipeline_commit_wr_en),
         .pipeline_commit_NPC      (pipeline_commit_NPC),
 
-        .if_NPC_dbg       (if_NPC_dbg),
-        .if_inst_dbg      (if_inst_dbg),
-        .if_valid_dbg     (if_valid_dbg),
-        .if_id_NPC_dbg    (if_id_NPC_dbg),
-        .if_id_inst_dbg   (if_id_inst_dbg),
-        .if_id_valid_dbg  (if_id_valid_dbg),
-        .id_ex_NPC_dbg    (id_ex_NPC_dbg),
-        .id_ex_inst_dbg   (id_ex_inst_dbg),
-        .id_ex_valid_dbg  (id_ex_valid_dbg),
-        .ex_mem_NPC_dbg   (ex_mem_NPC_dbg),
-        .ex_mem_inst_dbg  (ex_mem_inst_dbg),
-        .ex_mem_valid_dbg (ex_mem_valid_dbg),
-        .mem_wb_NPC_dbg   (mem_wb_NPC_dbg),
-        .mem_wb_inst_dbg  (mem_wb_inst_dbg),
-        .mem_wb_valid_dbg (mem_wb_valid_dbg)
+        // .if_NPC_dbg       (if_NPC_dbg),
+        // .if_inst_dbg      (if_inst_dbg),
+        // .if_valid_dbg     (if_valid_dbg),
+        // .if_id_NPC_dbg    (if_id_NPC_dbg),
+        // .if_id_inst_dbg   (if_id_inst_dbg),
+        // .if_id_valid_dbg  (if_id_valid_dbg),
+        // .id_ex_NPC_dbg    (id_ex_NPC_dbg),
+        // .id_ex_inst_dbg   (id_ex_inst_dbg),
+        // .id_ex_valid_dbg  (id_ex_valid_dbg),
+        // .ex_mem_NPC_dbg   (ex_mem_NPC_dbg),
+        // .ex_mem_inst_dbg  (ex_mem_inst_dbg),
+        // .ex_mem_valid_dbg (ex_mem_valid_dbg),
+        // .mem_wb_NPC_dbg   (mem_wb_NPC_dbg),
+        // .mem_wb_inst_dbg  (mem_wb_inst_dbg),
+        // .mem_wb_valid_dbg (mem_wb_valid_dbg)
+
+        .rs_table_dbg   (rs_table_dbg),
+        .X_packets_dbg  (X_packets_dbg),
+        .cdb_dbg        (cdb_dbg),
+        .busy_dbg       (busy_dbg)
     );
 
 
@@ -150,16 +163,16 @@ module testbench;
         // *Note that after this, all stdout output goes to visual debugger*
         // each argument is number of registers/signals for the group
         initcurses(
-            5,  // IF
-            4,  // IF/ID
-            13, // ID
-            17, // ID/EX
-            4,  // EX
-            14, // EX/MEM
-            5,  // MEM
-            9,  // MEM/WB
-            3,  // WB
-            2   // Miscellaneous
+            0,  // IF
+            0,  // IF/ID
+            0, // ID
+            0, // ID/EX
+            0,  // EX
+            0, // EX/MEM
+            0,  // MEM
+            0,  // MEM/WB
+            0,  // WB
+            4   // Miscellaneous
         );
 
         // Pulse the reset signal
@@ -213,21 +226,21 @@ module testbench;
         $display("z%h",reset);
 
         // Dump register file contents
-        $write("a");
-        for(int i = 0; i < 32; i=i+1) begin
-            $write("%h", pipeline_0.stage_id_0.regfile_0.registers[i]);
-        end
-        $display("");
+        // $write("a");
+        // for(int i = 0; i < 32; i=i+1) begin
+        //     $write("%h", pipeline_0.stage_id_0.regfile_0.registers[i]);
+        // end
+        // $display("");
 
         // Dump instructions and their validity for each stage
-        $write("p");
-        $write("%h%h%h%h%h%h%h%h%h%h ",
-               if_inst_dbg,      if_valid_dbg,
-               if_id_inst_dbg,   if_id_valid_dbg,
-               id_ex_inst_dbg,   id_ex_valid_dbg,
-               ex_mem_inst_dbg,  ex_mem_valid_dbg,
-               mem_wb_inst_dbg,  mem_wb_valid_dbg);
-        $display("");
+        // $write("p");
+        // $write("%h%h%h%h%h%h%h%h%h%h ",
+        //        if_inst_dbg,      if_valid_dbg,
+        //        if_id_inst_dbg,   if_id_valid_dbg,
+        //        id_ex_inst_dbg,   id_ex_valid_dbg,
+        //        ex_mem_inst_dbg,  ex_mem_valid_dbg,
+        //        mem_wb_inst_dbg,  mem_wb_valid_dbg);
+        // $display("");
 
         // Dump interesting register/signal contents onto stdout
         // format is "<reg group prefix><name> <width in hex chars>:<data>"
@@ -235,102 +248,120 @@ module testbench;
         // f: IF   d: ID   e: EX   m: MEM    w: WB  v: misc. reg
         // g: IF/ID   h: ID/EX  i: EX/MEM  j: MEM/WB
 
-        // IF signals (5) - prefix 'f'
-        $display("fNPC 8:%h",         pipeline_0.if_packet.NPC);
-        $display("finst 8:%h",        pipeline_0.if_packet.inst);
-        $display("fImem_addr 8:%h",   pipeline_0.stage_if_0.proc2Imem_addr);
-        $display("fPC_reg 8:%h",      pipeline_0.stage_if_0.PC_reg);
-        $display("fvalid 1:%h",       pipeline_0.if_packet.valid);
+        // // IF signals (5) - prefix 'f'
+        // $display("fNPC 8:%h",         pipeline_0.if_packet.NPC);
+        // $display("finst 8:%h",        pipeline_0.if_packet.inst);
+        // $display("fImem_addr 8:%h",   pipeline_0.stage_if_0.proc2Imem_addr);
+        // $display("fPC_reg 8:%h",      pipeline_0.stage_if_0.PC_reg);
+        // $display("fvalid 1:%h",       pipeline_0.if_packet.valid);
 
-        // IF/ID signals (4) - prefix 'g'
-        $display("genable 1:%h",      pipeline_0.if_id_enable);
-        $display("gNPC 16:%h",        pipeline_0.if_id_reg.NPC);
-        $display("ginst 8:%h",        pipeline_0.if_id_reg.inst);
-        $display("gvalid 1:%h",       pipeline_0.if_id_reg.valid);
+        // // IF/ID signals (4) - prefix 'g'
+        // $display("genable 1:%h",      pipeline_0.if_id_enable);
+        // $display("gNPC 16:%h",        pipeline_0.if_id_reg.NPC);
+        // $display("ginst 8:%h",        pipeline_0.if_id_reg.inst);
+        // $display("gvalid 1:%h",       pipeline_0.if_id_reg.valid);
 
-        // ID signals (13) - prefix 'd'
-        $display("drs1 8:%h",         pipeline_0.id_packet.rs1_value);
-        $display("drs2 8:%h",         pipeline_0.id_packet.rs2_value);
-        $display("ddest_reg 2:%h",    pipeline_0.id_packet.dest_reg_idx);
-        $display("drd_mem 1:%h",      pipeline_0.id_packet.rd_mem);
-        $display("dwr_mem 1:%h",      pipeline_0.id_packet.wr_mem);
-        $display("dopa_sel 1:%h",     pipeline_0.id_packet.opa_select);
-        $display("dopb_sel 1:%h",     pipeline_0.id_packet.opb_select);
-        $display("dalu_func 2:%h",    pipeline_0.id_packet.alu_func);
-        $display("dcond_br 1:%h",     pipeline_0.id_packet.cond_branch);
-        $display("duncond_br 1:%h",   pipeline_0.id_packet.uncond_branch);
-        $display("dhalt 1:%h",        pipeline_0.id_packet.halt);
-        $display("dillegal 1:%h",     pipeline_0.id_packet.illegal);
-        $display("dvalid 1:%h",       pipeline_0.id_packet.valid);
+        // // ID signals (13) - prefix 'd'
+        // $display("drs1 8:%h",         pipeline_0.id_packet.rs1_value);
+        // $display("drs2 8:%h",         pipeline_0.id_packet.rs2_value);
+        // $display("ddest_reg 2:%h",    pipeline_0.id_packet.dest_reg_idx);
+        // $display("drd_mem 1:%h",      pipeline_0.id_packet.rd_mem);
+        // $display("dwr_mem 1:%h",      pipeline_0.id_packet.wr_mem);
+        // $display("dopa_sel 1:%h",     pipeline_0.id_packet.opa_select);
+        // $display("dopb_sel 1:%h",     pipeline_0.id_packet.opb_select);
+        // $display("dalu_func 2:%h",    pipeline_0.id_packet.alu_func);
+        // $display("dcond_br 1:%h",     pipeline_0.id_packet.cond_branch);
+        // $display("duncond_br 1:%h",   pipeline_0.id_packet.uncond_branch);
+        // $display("dhalt 1:%h",        pipeline_0.id_packet.halt);
+        // $display("dillegal 1:%h",     pipeline_0.id_packet.illegal);
+        // $display("dvalid 1:%h",       pipeline_0.id_packet.valid);
 
-        // ID/EX signals (17) - prefix 'h'
-        $display("henable 1:%h",      pipeline_0.id_ex_enable);
-        $display("hNPC 16:%h",        pipeline_0.id_ex_reg.NPC);
-        $display("hinst 8:%h",        pipeline_0.id_ex_reg.inst);
-        $display("hrs1 8:%h",         pipeline_0.id_ex_reg.rs1_value);
-        $display("hrs2 8:%h",         pipeline_0.id_ex_reg.rs2_value);
-        $display("hdest_reg 2:%h",    pipeline_0.id_ex_reg.dest_reg_idx);
-        $display("hrd_mem 1:%h",      pipeline_0.id_ex_reg.rd_mem);
-        $display("hwr_mem 1:%h",      pipeline_0.id_ex_reg.wr_mem);
-        $display("hopa_sel 1:%h",     pipeline_0.id_ex_reg.opa_select);
-        $display("hopb_sel 1:%h",     pipeline_0.id_ex_reg.opb_select);
-        $display("halu_func 2:%h",    pipeline_0.id_ex_reg.alu_func);
-        $display("hcond_br 1:%h",     pipeline_0.id_ex_reg.cond_branch);
-        $display("huncond_br 1:%h",   pipeline_0.id_ex_reg.uncond_branch);
-        $display("hhalt 1:%h",        pipeline_0.id_ex_reg.halt);
-        $display("hillegal 1:%h",     pipeline_0.id_ex_reg.illegal);
-        $display("hvalid 1:%h",       pipeline_0.id_ex_reg.valid);
-        $display("hcsr_op 1:%h",      pipeline_0.id_ex_reg.csr_op);
+        // // ID/EX signals (17) - prefix 'h'
+        // $display("henable 1:%h",      pipeline_0.id_ex_enable);
+        // $display("hNPC 16:%h",        pipeline_0.id_ex_reg.NPC);
+        // $display("hinst 8:%h",        pipeline_0.id_ex_reg.inst);
+        // $display("hrs1 8:%h",         pipeline_0.id_ex_reg.rs1_value);
+        // $display("hrs2 8:%h",         pipeline_0.id_ex_reg.rs2_value);
+        // $display("hdest_reg 2:%h",    pipeline_0.id_ex_reg.dest_reg_idx);
+        // $display("hrd_mem 1:%h",      pipeline_0.id_ex_reg.rd_mem);
+        // $display("hwr_mem 1:%h",      pipeline_0.id_ex_reg.wr_mem);
+        // $display("hopa_sel 1:%h",     pipeline_0.id_ex_reg.opa_select);
+        // $display("hopb_sel 1:%h",     pipeline_0.id_ex_reg.opb_select);
+        // $display("halu_func 2:%h",    pipeline_0.id_ex_reg.alu_func);
+        // $display("hcond_br 1:%h",     pipeline_0.id_ex_reg.cond_branch);
+        // $display("huncond_br 1:%h",   pipeline_0.id_ex_reg.uncond_branch);
+        // $display("hhalt 1:%h",        pipeline_0.id_ex_reg.halt);
+        // $display("hillegal 1:%h",     pipeline_0.id_ex_reg.illegal);
+        // $display("hvalid 1:%h",       pipeline_0.id_ex_reg.valid);
+        // $display("hcsr_op 1:%h",      pipeline_0.id_ex_reg.csr_op);
 
-        // EX signals (4) - prefix 'e'
-        $display("eopa_mux 8:%h",     pipeline_0.stage_ex_0.opa_mux_out);
-        $display("eopb_mux 8:%h",     pipeline_0.stage_ex_0.opb_mux_out);
-        $display("ealu_result 8:%h",  pipeline_0.ex_packet.alu_result);
-        $display("etake_branch 1:%h", pipeline_0.ex_packet.take_branch);
+        // // EX signals (4) - prefix 'e'
+        // $display("eopa_mux 8:%h",     pipeline_0.stage_ex_0.opa_mux_out);
+        // $display("eopb_mux 8:%h",     pipeline_0.stage_ex_0.opb_mux_out);
+        // $display("ealu_result 8:%h",  pipeline_0.ex_packet.alu_result);
+        // $display("etake_branch 1:%h", pipeline_0.ex_packet.take_branch);
 
-        // EX/MEM signals (14) - prefix 'i'
-        $display("ienable 1:%h",      pipeline_0.ex_mem_enable);
-        $display("iNPC 8:%h",         pipeline_0.ex_mem_reg.NPC);
-        $display("iinst 8:%h",        pipeline_0.ex_mem_inst_dbg);
-        $display("irs2 8:%h",         pipeline_0.ex_mem_reg.rs2_value);
-        $display("ialu_result 8:%h",  pipeline_0.ex_mem_reg.alu_result);
-        $display("idest_reg 2:%h",    pipeline_0.ex_mem_reg.dest_reg_idx);
-        $display("ird_mem 1:%h",      pipeline_0.ex_mem_reg.rd_mem);
-        $display("iwr_mem 1:%h",      pipeline_0.ex_mem_reg.wr_mem);
-        $display("itake_branch 1:%h", pipeline_0.ex_mem_reg.take_branch);
-        $display("ihalt 1:%h",        pipeline_0.ex_mem_reg.halt);
-        $display("iillegal 1:%h",     pipeline_0.ex_mem_reg.illegal);
-        $display("ivalid 1:%h",       pipeline_0.ex_mem_reg.valid);
-        $display("icsr_op 1:%h",      pipeline_0.ex_mem_reg.csr_op);
-        // haven't updated VTUBER to use rd_unsigned yet
-        $display("imem_size 1:%h",    {pipeline_0.ex_mem_reg.rd_unsigned, pipeline_0.ex_mem_reg.mem_size});
+        // // EX/MEM signals (14) - prefix 'i'
+        // $display("ienable 1:%h",      pipeline_0.ex_mem_enable);
+        // $display("iNPC 8:%h",         pipeline_0.ex_mem_reg.NPC);
+        // $display("iinst 8:%h",        pipeline_0.ex_mem_inst_dbg);
+        // $display("irs2 8:%h",         pipeline_0.ex_mem_reg.rs2_value);
+        // $display("ialu_result 8:%h",  pipeline_0.ex_mem_reg.alu_result);
+        // $display("idest_reg 2:%h",    pipeline_0.ex_mem_reg.dest_reg_idx);
+        // $display("ird_mem 1:%h",      pipeline_0.ex_mem_reg.rd_mem);
+        // $display("iwr_mem 1:%h",      pipeline_0.ex_mem_reg.wr_mem);
+        // $display("itake_branch 1:%h", pipeline_0.ex_mem_reg.take_branch);
+        // $display("ihalt 1:%h",        pipeline_0.ex_mem_reg.halt);
+        // $display("iillegal 1:%h",     pipeline_0.ex_mem_reg.illegal);
+        // $display("ivalid 1:%h",       pipeline_0.ex_mem_reg.valid);
+        // $display("icsr_op 1:%h",      pipeline_0.ex_mem_reg.csr_op);
+        // // haven't updated VTUBER to use rd_unsigned yet
+        // $display("imem_size 1:%h",    {pipeline_0.ex_mem_reg.rd_unsigned, pipeline_0.ex_mem_reg.mem_size});
 
-        // MEM signals (5) - prefix 'm'
-        $display("mmem_data 16:%h",   pipeline_0.mem2proc_data);
-        $display("mmem_result 8:%h",  pipeline_0.mem_wb_reg.result);
-        $display("m2Dmem_data 16:%h", pipeline_0.proc2mem_data);
-        $display("m2Dmem_addr 8:%h",  pipeline_0.proc2Dmem_addr);
-        $display("m2Dmem_cmd 1:%h",   pipeline_0.proc2Dmem_command);
+        // // MEM signals (5) - prefix 'm'
+        // $display("mmem_data 16:%h",   pipeline_0.mem2proc_data);
+        // $display("mmem_result 8:%h",  pipeline_0.mem_wb_reg.result);
+        // $display("m2Dmem_data 16:%h", pipeline_0.proc2mem_data);
+        // $display("m2Dmem_addr 8:%h",  pipeline_0.proc2Dmem_addr);
+        // $display("m2Dmem_cmd 1:%h",   pipeline_0.proc2Dmem_command);
 
-        // MEM/WB signals (9) - prefix 'j'
-        $display("jenable 1:%h",      pipeline_0.mem_wb_enable);
-        $display("jNPC 8:%h",         pipeline_0.mem_wb_NPC_dbg);
-        $display("jinst 8:%h",        pipeline_0.mem_wb_inst_dbg);
-        $display("jresult 8:%h",      pipeline_0.mem_wb_reg.result);
-        $display("jdest_reg 2:%h",    pipeline_0.mem_wb_reg.dest_reg_idx);
-        $display("jtake_branch 1:%h", pipeline_0.mem_wb_reg.take_branch);
-        $display("jhalt 1:%h",        pipeline_0.mem_wb_reg.halt);
-        $display("jillegal 1:%h",     pipeline_0.mem_wb_reg.illegal);
-        $display("jvalid 1:%h",       pipeline_0.mem_wb_reg.valid);
+        // // MEM/WB signals (9) - prefix 'j'
+        // $display("jenable 1:%h",      pipeline_0.mem_wb_enable);
+        // $display("jNPC 8:%h",         pipeline_0.mem_wb_NPC_dbg);
+        // $display("jinst 8:%h",        pipeline_0.mem_wb_inst_dbg);
+        // $display("jresult 8:%h",      pipeline_0.mem_wb_reg.result);
+        // $display("jdest_reg 2:%h",    pipeline_0.mem_wb_reg.dest_reg_idx);
+        // $display("jtake_branch 1:%h", pipeline_0.mem_wb_reg.take_branch);
+        // $display("jhalt 1:%h",        pipeline_0.mem_wb_reg.halt);
+        // $display("jillegal 1:%h",     pipeline_0.mem_wb_reg.illegal);
+        // $display("jvalid 1:%h",       pipeline_0.mem_wb_reg.valid);
 
-        // WB signals (3) - prefix 'w'
-        $display("wwr_data 8:%h",     pipeline_0.wb_regfile_data);
-        $display("wwr_idx 2:%h",      pipeline_0.wb_regfile_idx);
-        $display("wwr_en 1:%h",       pipeline_0.wb_regfile_en);
+        // // WB signals (3) - prefix 'w'
+        // $display("wwr_data 8:%h",     pipeline_0.wb_regfile_data);
+        // $display("wwr_idx 2:%h",      pipeline_0.wb_regfile_idx);
+        // $display("wwr_en 1:%h",       pipeline_0.wb_regfile_en);
 
         // Misc signals(2) - prefix 'v'
-        $display("vcompleted 1:%h",   pipeline_completed_insts);
-        $display("vpipe_err 1:%h",    pipeline_error_status);
+        // $display("vcompleted 1:%h",   pipeline_completed_insts);
+        // $display("vpipe_err 1:%h",    pipeline_error_status);
+        // Show CDB state
+        $display("vCDB_valid 1:%h", cdb_dbg.valid);
+        $display("vCDB_T 2:%h", cdb_dbg.T);
+        $display("vCDB_V 4:%h", cdb_dbg.V);
+
+        // For busy bits, show them as a single value
+        $display("vRS_busy 2:%h", busy_dbg);
+
+
+        // Show key info for busy entries
+        for(int i = 0; i < `RS_SZ; i++) begin
+        if(busy_dbg[i]) begin
+            $display("vRS%0d_T 2:%h", i, rs_table_dbg[i].T);
+            $display("vRS%0d_T1 2:%h", i, rs_table_dbg[i].T1);
+            $display("vRS%0d_T2 2:%h", i, rs_table_dbg[i].T2);
+            $display("vRS%0d_rdy 1:%h", i, rs_table_dbg[i].ready);
+        end
+        end
 
         // must come last
         $display("break");
