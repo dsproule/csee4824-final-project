564a6e21e18d75563ccb4528ec1174de682aee88
Donovan <Donovan.sproule@yahoo.com>
2025-04-08 23:02:27 -0400

    Completed the loading. Can now handle variable amounts of reading. Also bug in pipeline with RS that got cleared along the way

--- verilog/func_unit_2.sv
+++ verilog/func_unit_2.sv
@@ -11,40 +11,45 @@ module func_unit_2(
     output [`XLEN-1:0] proc2Dmem_addr,
     output X_C_PACKET X_packet
 );
-    logic [`XLEN-1:0] read_data;
-    logic [`XLEN-1:0] Dmem_addr_raw;
-    logic [3:0]  nextImem_tag;
-    mem_proc_states mem_state;
+    logic [`XLEN-1:0] rawDmem_addr, shifted_result;
+    logic [5:0]       shift;
+    logic [63:0]      Dmem_data;
+    logic [3:0]       nextImem_tag, line_offset;
+    mem_proc_states   mem_state;
 
     // should be word-aligned. If a value is invalid proc_resp will be 0
-    assign Dmem_addr_raw  = S_X_reg.V1 + S_X_reg.mem_offset;
-    assign proc2Dmem_addr = {Dmem_addr_raw[`XLEN-1:3], 3'b0};
+    assign rawDmem_addr   = S_X_reg.V1 + S_X_reg.mem_offset;
+    assign proc2Dmem_addr = {rawDmem_addr[`XLEN-1:3], 3'b0};
+    assign line_offset    = rawDmem_addr[2:0];
 
     always_comb begin
-        read_data = '0;
+        // perform the offset shift here to apply the masks. Uses shifting to avoid multiplication
+        shift = (S_X_reg.mem_size == BYTE) ? (line_offset)         << 3 :        // turns bytes to num of bits to shift by
+                (S_X_reg.mem_size == HALF) ? (line_offset >> 1)    << 4 :        // floor(2)  --> grabs by granularity of 2 bytes at a time
+                (S_X_reg.mem_size == WORD) ? (line_offset[2] << 2) << 3 : 0;     // if (line_offset >= 4) shift by a word --> upper 32 bits
+        
+        Dmem_data = Dmem2proc_data >> shift;
+
         if (S_X_reg.rd_unsigned) begin
             if (S_X_reg.mem_size == BYTE)
-                read_data[7:0] = Dmem2proc_data[7:0];
+                Dmem_data[`XLEN-1:8] = 0;
             else if (S_X_reg.mem_size == HALF)
-                read_data[15:0] = Dmem2proc_data[15:0];
-            else
-                read_data = Dmem2proc_data[`XLEN-1:0];
+                Dmem_data[`XLEN-1:16] = 0;
         end else begin
             if (S_X_reg.mem_size == BYTE)
-                read_data = {{(`XLEN-8){Dmem2proc_data[7]}}, Dmem2proc_data[7:0]};
+                Dmem_data[`XLEN-1:8] = {(`XLEN-8){Dmem2proc_data[7 + shift]}};
             else if (S_X_reg.mem_size == HALF)
-                read_data = {{(`XLEN-16){Dmem2proc_data[15]}}, Dmem2proc_data[15:0]};
-            else
-                read_data = Dmem2proc_data[`XLEN-1:0];
-
+                Dmem_data[`XLEN-1:16] = {(`XLEN-16){Dmem2proc_data[15 + shift]}};
         end
+        shifted_result = Dmem_data[`XLEN-1:0];
     end
 
     // state machine to handle loads
     always_ff @(posedge clock) begin
         if (reset) begin
-            nextImem_tag <= '0;
-            mem_state <= MEM_WAIT_FOR_ADDR;
+            X_packet      <= '0;
+            nextImem_tag  <= '0;
+            mem_state     <= MEM_WAIT_FOR_ADDR;
             mem_load_pend <= `FALSE;
         end else begin
             case (mem_state)
@@ -66,7 +71,7 @@ module func_unit_2(
                     // if the memory is responding to us, save it and wait to be retired
                     if (mem2proc_tag == nextImem_tag) begin
                         X_packet.T         <= S_X_reg.T;
-                        X_packet.result    <= read_data;
+                        X_packet.result    <= shifted_result;
                         X_packet.ppln_ctrl <= '0;
                         X_packet.ppln_ctrl.has_dest <= `TRUE;
                         X_packet.valid     <= `TRUE;
