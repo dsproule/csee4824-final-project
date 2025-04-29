0ed539a3407ed97c1a7e94a9bc2e2c56e7125989
Nicolas Alarcon <na2946@columbia.edu>
2025-04-29 16:05:35 -0400

    undo bad change with fu ready

--- verilog/pipeline.sv
+++ verilog/pipeline.sv
@@ -329,8 +329,6 @@ module pipeline (
             end else if (gnt[fu_idx]) begin
                 FU_ready[fu_idx] <= `TRUE;
             end
-            FU_ready[2] <= !lq_full;
-            FU_ready[3] <= !sq_full;
     end
     
     rob rob_inst (
